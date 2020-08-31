fff0d5f9b99339d82e700de2b5007bc3c17b44bc
JerryHanz <avianhermana@gmail.com>
2020-08-31 07:00:05 +0700

    test

---
+++ app/Http/Controllers/AdminLTEController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminLTEController extends Controller
+{
+    //
+    public function index(){
+        return view ('AdminLTE/index');
+    }
+}
